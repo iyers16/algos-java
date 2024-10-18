3e3ca8f1f2ae1f4284515a26f798ac188f9adf8e
Shanmukh Iyer <shanmukh.iyer@gmail.com>
2024-10-18 16:42:36 -0400

    selection + bubble done

---
+++ Algos.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class Algos {
+    public static void selectionSort(int[] arr) {
+        printArray(arr);
+        for (int i = 0; i < arr.length; ++i) {
+            int unsortedIdx = i + 1;
+            int minIdx = i;
+
+            for (int j = unsortedIdx; j < arr.length; ++j) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+        printArray(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        printArray(arr);
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr.length - 1; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+    public static void swap(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+        if (isSorted(arr)) {
+            System.out.println("Sorted");
+        } else {
+            System.out.println("Unsorted");
+        }
+    }
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false; // Return false if an element is greater than the next one
+            }
+        }
+        return true; // Return true if no such element is found
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] unsortedArray = IntStream.generate(() -> random.nextInt(101))
+                .limit(10)
+                .toArray();
+        Algos.selectionSort(unsortedArray);
+        Algos.bubbleSort(unsortedArray);
+    }
+}
